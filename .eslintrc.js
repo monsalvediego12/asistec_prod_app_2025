cad33093e94f122240ebaac671f7bbbc6a8a4b3a
Diego Monsalve <diegomonsalve@MacBook-Air-de-Diego.local>
2025-09-08 12:23:52 -0500

    app settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    'react/no-unstable-nested-components': [
+      'off' || 'warn' || 'error',
+      {
+        allowAsProps: true || false,
+        customValidators:
+          [] /* optional array of validators used for propTypes validation */,
+      },
+    ],
+  },  
 };
